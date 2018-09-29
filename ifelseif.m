8ad2b107a910f4a641189962f34de918117a6ae6
Liliana Jaime Loyola <43586912+LOYOLA2018@users.noreply.github.com>
2018-09-28 23:44:21 -0500

    Add files via upload

---
+++ ifelseif.m
@@ -0,0 +1,13 @@
+prompt = 'introduzca el numero A';
+A = input(prompt);
+
+disp("introduzca el numero B");
+B = input(prompt);
+
+if(A>B)
+    prompt("A es mayor que B");
+elseif (A<B)
+        prompt("A es menor que B");
+        else
+            prompt("A es igual a B");
+        end            
